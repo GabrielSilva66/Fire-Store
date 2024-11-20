26d9b434b3882c5331d31d0c4b8ee41eb82aec1d
Gabriel Victor <gabrieltedz.com.@gmail.com>
2024-11-19 21:40:47 -0300

    Feat: Create StateController

--- src/main/java/com/project/system/controller/StateController.java
+++ src/main/java/com/project/system/controller/StateController.java
@@ -6,10 +6,18 @@ import com.project.system.repositories.StateRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.integration.IntegrationProperties;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
 
 @Controller
 public class StateController {
@@ -21,6 +29,38 @@ public class StateController {
     public ModelAndView register (State state) {
         ModelAndView mv = new ModelAndView("management/state/register");
         return mv.addObject("state", state);
+    }
+
+    @GetMapping("/stateList")
+    public ModelAndView listActiveStates() {
+        ModelAndView mv = new ModelAndView("/management/state/list");
+
+        // Buscar apenas estados ativos
+        List<State> activeStates = stateRepository.findActiveStates();
+        mv.addObject("stateList", activeStates);
+        return mv;
+    }
+
+    @GetMapping("/editState/{id}")
+    public ModelAndView edit(@PathVariable("id") UUID id){
+        Optional<State> state = stateRepository.findById(id);
+        stateRepository.updateUpdateIn(id, LocalDateTime.now());
+        return register(state.get());
+    }
+
+
+    @GetMapping("/deleteState/{id}")
+    public ModelAndView deactivateState(@PathVariable("id") UUID id, RedirectAttributes redirectAttributes) {
+        Optional<State> state = stateRepository.findById(id);
+
+        if (state.isPresent()) {
+            stateRepository.updateStateStatus(id, false);  // Desativa o estado (false)
+            redirectAttributes.addFlashAttribute("message", "Estado marcado como inativo com sucesso!");
+        } else {
+            redirectAttributes.addFlashAttribute("error", "Estado não encontrado!");
+        }
+
+        return listActiveStates();
     }
 
     @PostMapping("/saveState")
--- src/main/resources/db/migration/V1__create-state-table.sql
+++ src/main/resources/db/migration/V1__create-state-table.sql
@@ -5,6 +5,10 @@ CREATE TABLE state(
     id UUID DEFAULT gen_random_uuid() PRIMARY KEY,
     name varchar(100) NOT NULL,
     acronym varchar(5) NOT NULL,
-    create_in timestamp,
-    update_in timestamp
+    is_active BOOLEAN NOT NULL DEFAULT TRUE,
+    create_in timestamp DEFAULT NOW(),
+    update_in timestamp DEFAULT NOW()
+
 );
+
+
