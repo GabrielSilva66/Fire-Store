88ba9712e38fdab8b8ef43cbbb1e8d2ca63180ed
Gabriel Victor <gabrieltedz.com.@gmail.com>
2025-03-26 19:05:34 -0300

    feat: update repository

---
+++ src/main/java/com/project/system/repositories/IntakeRepository.java
@@ -0,0 +1,8 @@
+package com.project.system.repositories;
+
+import com.project.system.models.Intake;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface IntakeRepository extends JpaRepository<Intake, Long> {
+
+}
--- src/main/java/com/project/system/repositories/ProductRepository.java
+++ src/main/java/com/project/system/repositories/ProductRepository.java
@@ -1,7 +1,20 @@
 package com.project.system.repositories;
 
+import com.project.system.models.Customer;
 import com.project.system.models.Intake;
+import com.project.system.models.Product;
+import jakarta.transaction.Transactional;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
-public interface ProductRepository extends JpaRepository<Intake, Long> {
+import java.util.List;
+
+public interface ProductRepository extends JpaRepository<Product, Long> {
+
+//    @Modifying
+//    @Transactional
+//    @Query(value = "UPDATE tb_product SET is_active = :status WHERE id = :id", nativeQuery = true)
+//    void updateProductStatus(@Param("id") Long id, @Param("status") boolean status);
 }
